c0ebc6fc4c83acfcbd331dc36f0a3fa63542b215
pahujashreya5 <pahujashreya5@gmail.com>
2026-07-15 11:34:54 +0530

    added ground plane and corrected bounding box extent

--- python-scripts/masterScene.py
+++ python-scripts/masterScene.py
@@ -19,6 +19,15 @@ def CreateStage(file_path):
     
     return stage
 
+def CreateGround(stage, prim_path):
+    # creates a ground plane (will be used to detect sphere hits)
+    ground=UsdGeom.Plane.Define(stage, prim_path)
+    xform=UsdGeom.Xformable(ground)
+    xform.ClearXformOpOrder()
+    translate_op=xform.AddTranslateOp()
+    translate_op.Set(Gf.Vec3d(0,0,0))
+    ground.GetPrim().SetMetadata('comment', 'created ground plane')
+
 # we give this prim node a path where it lives
 def CreateSphere(stage, prim_path):
     # Creates a sphere prim on the provided stage
@@ -31,11 +40,11 @@ def AnimateSphere(sphere_prim):
     # animating the sphere
     # transformation for sphere
     xform = UsdGeom.Xformable(sphere_prim)
-    
     # clearing any previous transformations
     xform.ClearXformOpOrder()
     # adding a transformation operation
     translate_op = xform.AddTranslateOp()
+    scale_op=xform.AddScaleOp()
     
     # interval set to 8 seconds
     start_frame=1
@@ -44,22 +53,24 @@ def AnimateSphere(sphere_prim):
     speed=8
     # by default uses linear interpolation
     for frame in range (start_frame, end_frame):
-        x=random.uniform(0.0,10.0)
-        y=random.uniform(0.0,10.0)
-        z=random.uniform(0.0,10.0)
+        x=random.uniform(-1.0,1.0)
+        y=random.uniform(-1.0,1.0)
+        # to make sure we have hits
+        z=random.uniform(0.999,2.0)
         time_code=Usd.TimeCode(frame*speed)
         # author opinions at specific timecodes
         # Gf.Vec3d is used to pass precise 3D vector coordinates (x,y,z)
         translate_op.Set(Gf.Vec3d(x,y,z),time_code)
-
+        
     sphere_prim.GetPrim().SetMetadata('comment', 'animated sphere')
 
 def main():
     # 1. Initialize the dataset in memory
     file_name = 'stage1.usda'
     stage = CreateStage(file_name)
-    
-    # 2. Pass the memory object through your modular functions
+
+    # 2. pass the memory object (stage usda) through created functions
+    ground=CreateGround(stage,'/Root/ground')
     sphere = CreateSphere(stage, '/Root/sphere')
     AnimateSphere(sphere)
     
