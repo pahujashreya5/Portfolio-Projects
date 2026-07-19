4caa20725c9fd4f6401219ca6079f0cb32353574
pahujashreya5 <pahujashreya5@gmail.com>
2026-07-19 23:03:18 +0530

    clean up

--- python-scripts/init_script.py
+++ python-scripts/init_script.py
@@ -1,15 +1,13 @@
 import os
-print("PYTHONPATH:", os.environ.get("PYTHONPATH"))
-print("PXR_PLUGINPATH_NAME:", os.environ.get("PXR_PLUGINPATH_NAME"))
-print("DYLD_LIBRARY_PATH:", os.environ.get("DYLD_LIBRARY_PATH"))
 from pxr import Usd, UsdGeom, Gf, Vt
 import random
 
 def CreateStage(file_path):
-    # Initializes the stage and sets base metadata
+    # init the stage
     if os.path.exists(file_path):
         os.remove(file_path)
 
+    # create a stage
     stage = Usd.Stage.CreateNew(file_path)
     
     # define default root prim
@@ -32,6 +30,7 @@ def CreateGround(stage, prim_path):
     # we need to author this manually in order to use bbox_cache later
     ground.CreateExtentAttr([Gf.Vec3f(-1, -1, -1), Gf.Vec3f(1, 1, 1)])
 
+    # use xform from UsdGeom for transform matrix
     xform=UsdGeom.Xformable(ground)
     xform.ClearXformOpOrder()
     translate_op=xform.AddTranslateOp()
@@ -55,16 +54,14 @@ def AnimateSphere(sphere_prim):
     # animating the sphere
     # transformation for sphere
     xform = UsdGeom.Xformable(sphere_prim)
-    # clearing any previous transformations
-    # xform.ClearXformOpOrder()
     # adding a transformation operation
     translate_op = xform.AddTranslateOp()
     
     # interval set to 8 seconds
     start_frame=1
     end_frame=192
-    # speed to implement a time step (increase for slower movement)
-    speed=8
+    # # speed to implement a time step (increase for slower movement)
+    # speed=8
     # by default uses linear interpolation
     for frame in range (start_frame, end_frame):
         x=random.uniform(-1.0,1.0)
@@ -79,19 +76,18 @@ def AnimateSphere(sphere_prim):
     sphere_prim.GetPrim().SetMetadata('comment', 'animated sphere')
 
 def main():
-    # 1. Initialize the dataset in memory
+    # init in memory
     file_name = 'stage1.usda'
     stage = CreateStage(file_name)
 
-    # 2. pass the memory object (stage usda) through created functions
-    # we need to create the ground too
+    # pass the memory object (stage.usda) through created functions
     ground = CreateGround(stage, '/Root/ground')
     sphere = CreateSphere(stage, '/Root/sphere')
     AnimateSphere(sphere)
     
     # commit the final scenegraph to memory
     stage.GetRootLayer().Save()
-    print(f"Successfully generated {file_name}")
+    print(f'Successfully generated {file_name}')
 
 if __name__ == '__main__':
     main()
